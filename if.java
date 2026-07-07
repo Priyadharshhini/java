5e276f79d4bf8ac0503c15cab937f8a1959bdd06
Priyadharshhini <priyadhans2007@gmail.com>
2026-07-07 19:35:40 +0530

    Add files via upload

---
+++ if.java
@@ -0,0 +1,12 @@
+public class numberseries{
+public static void main(String[] args){
+  int a=59;
+  int b=25;
+  if(a>b){
+    System.out.println("a is greater");
+  }
+  else{
+    System.out.println("b is greater");
+  }
+}
+}
